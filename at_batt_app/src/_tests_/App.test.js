9b4f429a18ab3f467c895f528f59533551d39ce1
badCompany55 <zachirvin45@gmail.com>
2019-04-13 14:28:42 -0500

    TESTING add display tests add dashboard tests

---
+++ at_batt_app/src/_tests_/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App.js';
+
+import {render} from 'react-testing-library'
+
+xit('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+	render(<App/>)
+})
